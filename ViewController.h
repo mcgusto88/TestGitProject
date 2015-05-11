38a3578dc45af2beffed4ada6c0a331137f54e5a
Nutech Systems <nutechsystems@MacBook-Pro.local>
2015-05-11 16:06:47 -0400

    Table view controller

---
+++ ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TestGitExercise
+//
+//  Created by Nutech Systems on 5/11/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) NSString *name;
+
+@end
+
