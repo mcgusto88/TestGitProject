50a4ab2f47ee4118ca2c7d5ab31e717117c6034f
Nutech Systems <nutechsystems@MacBook-Pro.local>
2015-05-11 16:08:39 -0400

    new property in table view controller

---
+++ TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  TestGitExercise
+//
+//  Created by Nutech Systems on 5/11/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic) int index;
+
+@end
